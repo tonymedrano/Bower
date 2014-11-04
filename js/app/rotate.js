5756d44161d4a0a50a1bc8fedc1597517bdb0840
Medrano, Tony (NYC-MRM) <Tony.Medrano@mrm-mccann.com>
2014-11-04 11:41:46 -0500

    Testing Bower package manager

---
+++ js/app/rotate.js
@@ -0,0 +1,26 @@
+var sheet = (function() {
+  var style = document.createElement('style');
+  style.appendChild(document.createTextNode(""));
+  document.head.appendChild(style);
+  return style.sheet;
+})();
+
+sheet.addRule('.ps-rotate-over', 'color:red;transform:rotate( -180deg );-moz-transform:rotate( -180deg );-ms-transform:rotate( -180deg );-o-transform:rotate( -180deg );-webkit-transform: rotate( -180deg );transition:transform 550ms ease;-moz-transition:-moz-transform 550ms ease;-ms-transition:-ms-transform 550ms ease;-o-transition:-o-transform 550ms ease;-webkit-transition: -webkit-transform 550ms ease;');
+sheet.addRule('.ps-rotate-out', 'transform:rotate( -360deg );-moz-transform:rotate( -360deg );-ms-transform:rotate( -360deg );-o-transform:rotate( -360deg );-webkit-transform: rotate( -360deg );transition:transform 550ms ease;-moz-transition:-moz-transform 550ms ease;-ms-transition:-ms-transform 550ms ease;-o-transition:-o-transform 550ms ease;-webkit-transition:-webkit-transform 550ms ease;')
+
+
+var rotateEls = document.getElementsByClassName('rotate');
+for(var i=0; i<rotateEls.length; i++) {
+  rotateEls[i].addEventListener( 'mouseover', function () {
+
+    this.className = 'ps-rotate-over';
+
+  }, false );
+
+  rotateEls[i].addEventListener( 'mouseout', function () {
+    var rotate = this;
+    rotate.className = 'ps-rotate-out';
+    window.setTimeout( function () { rotate.className = '' }, 550 );
+  }, false );
+}
+
